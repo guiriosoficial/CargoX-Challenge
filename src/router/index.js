56b157e1638dfd7972dd4f3cb6a139772e6cd9d1
riwersxd <guilherme.rc.98@gmail.com>
2021-03-16 02:21:58 -0300

    :alien: Create routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+
+const Deliveries = () => import('@/views/deliveries/Deliveries.vue' /* webpackChunkName: 'Deliveries' */)
+const Delivery = () => import('@/views/delivery/Delivery.vue' /* webpackChunkName: 'Delivery' */)
+const NotFound = () => import('@/views/NotFound.vue' /* webpackChunkName: 'NotFound' */)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Deliveries',
+    component: Deliveries
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/delivery/:id',
+    name: 'Delivery',
+    component: Delivery
+  },
+  {
+    path: '/delivery',
+    redirect: '/'
+  },
+  {
+    path: '/:catchAll(.*)',
+    component: NotFound
   }
 ]
 
