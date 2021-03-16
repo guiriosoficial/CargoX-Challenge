45aa709e770205ca17d1cacc6efa4f47ec6e9189
riwersxd <guilherme.rc.98@gmail.com>
2021-03-16 02:28:37 -0300

    :sparkles: Crete generic components

---
+++ src/components/Button.vue
@@ -0,0 +1,35 @@
+<template>
+  <span class="button-container">{{ $t(`buttons.${placeholder}`) }}</span>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+  props: {
+    placeholder: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables/colors.scss';
+
+.button-container {
+  cursor: pointer;
+  font-size: 0.83em;
+  border-radius: 6px;
+  padding: 4px 6px;
+  transition: .2s ease-out;
+  color: $--color-text-placeholder;
+  border: 1px solid $--color-text-placeholder;
+
+  &:hover {
+    background-color: $--color-info;
+    border: 1px solid $--color-info;
+    color: $--color-white;
+  }
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <header class="header-container">
+    <h1>
+      {{ String(title).toUpperCase() }}
+    </h1>
+    <h4 class="header-container__sub-title">
+      {{ subtitle }}
+    </h4>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'Header',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables/colors';
+
+.header-container {
+  margin: 30px 0;
+
+  .header-container__sub-title {
+    font-weight: 500;
+  }
+}
+
+</style>
