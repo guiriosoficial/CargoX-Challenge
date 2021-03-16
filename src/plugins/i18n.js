c118d421770f0174f7bec3605ec4fd77a0b7bdc6
riwersxd <guilherme.rc.98@gmail.com>
2021-03-16 03:40:05 -0300

    :art: Format code

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -18,7 +18,7 @@ function loadLocaleMessages () {
 
 const i18n = createI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'pt-br',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'pt-br',
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages: loadLocaleMessages()
 })
 
