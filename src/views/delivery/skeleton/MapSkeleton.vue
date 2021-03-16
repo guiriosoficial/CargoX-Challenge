5a213bf426ea544c27daa7a6fb8fcb7d32e9cc62
riwersxd <guilherme.rc.98@gmail.com>
2021-03-16 02:36:46 -0300

    :children_crossing: Create skeleton components

---
+++ src/views/delivery/skeleton/MapSkeleton.vue
@@ -0,0 +1,37 @@
+<template>
+  <aside class="map-container cx-card">
+    <map class="map-container__content skeleton-loading" />
+    <footer class="map-container__footer">
+      <span class="map-container__footer-placeholder skeleton-loading" />
+    </footer>
+  </aside>
+</template>
+
+<script>
+export default {
+  name: 'MapSkeleton'
+}
+</script>
+
+<style lang="scss" scoped>
+.map-container {
+  height: 100%;
+  padding: 0;
+
+  .map-container__content {
+    height: 480px;
+    width: 100%;
+  }
+
+  .map-container__footer {
+    padding: 32px 30px;
+
+    .map-container__footer-placeholder {
+      display: block;
+      margin-top: 1px;
+      height: 18px;
+      width: 240px;
+    }
+  }
+}
+</style>
