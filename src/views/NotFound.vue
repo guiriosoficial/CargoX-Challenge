0c0eca3de67b72831d2a61df83ce966563263aa6
riwersxd <guilherme.rc.98@gmail.com>
2021-03-16 02:29:34 -0300

    :goal_net: Create 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="error-container">
+    <h1>{{ $t('errors.404.code') }}</h1>
+    <h4>{{ $t('errors.404.message') }}</h4>
+    <Button
+      placeholder="go-back"
+      @click="$router.push('/')"
+    />
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+
+const Button = defineAsyncComponent(() => import('@/components/Button' /* webpackChunkName: 'Button' */))
+
+export default {
+  name: 'NotFound',
+  components: {
+    Button
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.error-container {
+  font-size: 1.4em;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 12px;
+}
+</style>
