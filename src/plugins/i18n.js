dfc6219a133ac4b6508724439f2b09ad9e880a5b
riwersxd <guilherme.rc.98@gmail.com>
2021-03-17 03:54:39 -0300

    🌐 Update internationalization config

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,25 +1,11 @@
 import { createI18n } from 'vue-i18n'
-
-function loadLocaleMessages () {
-  const locales = require.context('@/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  const messages = {}
-
-  locales.keys().forEach(key => {
-    const matched = key.match(/([a-z-9]+)\./i)
-
-    if (matched && matched.length > 1) {
-      const locale = matched[1]
-      messages[locale] = locales(key)
-    }
-  })
-
-  return messages
-}
+import pt_br from '@/locales/pt-br'
+import en from '@/locales/en'
 
 const i18n = createI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || 'pt-br',
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
-  messages: loadLocaleMessages()
+  messages: { en, 'pt-br': pt_br }
 })
 
 export default i18n
