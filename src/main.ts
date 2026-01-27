811323f8cace93b9efb10bd05026492fea89c957
Gui Rios <guilherme.rc.98@gmail.com>
2026-01-27 13:22:16 -0300

    🚚 Abstract Toastify

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,19 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import pinia from './store'
-import i18n from '@/plugins/i18n'
-import FaIcon from '@/plugins/fa'
-import toast, { toastOptions } from '@/plugins/toastfy'
-import './plugins/registerServiceWorker'
-
+import CxIcon from '@/components/CxIcon.vue'
+import App from '@/App.vue'
+import { router } from '@/router'
+import { pinia } from '@/store'
+import { i18n } from '@/plugins/i18n'
+import { vue3Toastify, toastOptions } from '@/plugins/notify'
+import '@/plugins/registerServiceWorker'
+import '@/plugins/fa'
 import '@/styles/core.scss'
 
 createApp(App)
   .use(pinia)
   .use(router)
   .use(i18n)
-  .use(toast, toastOptions)
-  .component('Icon', FaIcon)
+  .use(vue3Toastify, toastOptions)
+  .component('CxIcon', CxIcon)
   .mount('#app')
+
