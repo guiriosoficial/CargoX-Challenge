bfc90817f37886a78369c93595799687054cd2e7
Gui Rios <guilherme.rc.98@gmail.com>
2026-01-27 13:37:12 -0300

    ➕ Change datetime dependency to dayjs

--- src/filters/dateTime.ts
+++ src/filters/dateTime.ts
@@ -1,9 +1,10 @@
-import moment from 'moment'
+import dayjs from 'dayjs'
 
-function dateTime (dateTime: Date | string | number = new Date(), format = 'DD/MM/YYYY HH:mm'): string {
-  return moment(dateTime).isValid()
-    ? moment(dateTime).format(format)
+export function dateTime (
+  dateTime: Date | string | number = new Date(),
+  format = 'DD/MM/YYYY HH:mm'
+): string {
+  return dayjs(dateTime).isValid()
+    ? dayjs(dateTime).format(format)
     : String(dateTime)
 }
-
-export default dateTime
